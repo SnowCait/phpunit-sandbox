6a8213717a584ad8d52ad34aa99566974839bd15
SnowCait <SnowCait@users.noreply.github.com>
2019-10-04 00:37:09 +0900

    Update ATest.php

--- tests/ATest.php
+++ tests/ATest.php
@@ -6,6 +6,10 @@ class ATest extends TestCase
     public function test1()
     {
         sleep(5);
+        $pdo = new PDO('mysql:host=localhost;dbname=mysql', 'root', 'root');
+        $statement = $pdo->query('SELECT VERSION()');
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+        var_dump($row);
         $this->assertTrue(true);
     }
 }
