b9185e3ea50181e05b8891cf9bb979ebd3f04c85
SnowCait <SnowCait@users.noreply.github.com>
2019-10-04 00:28:29 +0900

    test

--- tests/ATest.php
+++ tests/ATest.php
@@ -5,6 +5,7 @@ class ATest extends TestCase
 {
     public function test1()
     {
+        sleep(5);
         $this->assertTrue(true);
     }
 }
--- tests/BTest.php
+++ tests/BTest.php
@@ -5,6 +5,7 @@ class BTest extends TestCase
 {
     public function test2()
     {
+        sleep(10);
         $this->assertTrue(true);
         $this->assertTrue(true);
     }
