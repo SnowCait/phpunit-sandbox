309ca052af2f97236d507bb5210e419c3fd48bf4
SnowCait <SnowCait@users.noreply.github.com>
2019-10-03 22:57:53 +0900

    test

---
+++ tests/test_1.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ATest extends TestCase
+{
+    public function test1()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/test_2.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class BTest extends TestCase
+{
+    public function test2()
+    {
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+    }
+}
